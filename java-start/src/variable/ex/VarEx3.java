7dad55eba956281bb9cb08d6d68e6caa1a8260be
ljs3536 <wlstjr320@naver.com>
2024-08-29 21:08:15 +0900

    문제풀이

---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long num = 10000000000L;
+        System.out.println(num);
+        boolean yn = true;
+        System.out.println(yn);
+    }
+}
